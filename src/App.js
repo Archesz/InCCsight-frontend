19739789c4294d41d366c3e379dc3984e89a2a7c
João Vitor <jv86679@gmail.com>
2022-08-14 01:32:53 -0300

    template inccsight

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './styles/global.scss'
 
 import Home from './pages/Home'
-import Main from './pages/Main'
 
 function App() {
 
   return (
     <div className="App">
         
-        <Main />
+        <Home />
 
     </div>
   );
