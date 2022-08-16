f9928369e8b57537412ae770ec12078d69d1bead
Archesz <jv86679@gmail.com>
2022-08-16 13:20:11 -0300

    call tool fixed

--- public/script.js
+++ public/script.js
@@ -9,3 +9,12 @@ function teste(){
     
     console.log('okkkk')
 }
+
+function callTool(path){
+    if(process.platform == "linux"){
+        console.log("LINUX detected")
+        // console.log("python app.py -f /home/jovi/Data/subject_01")
+        command = exec(`cd .. && cd inCCsight && python app.py -f ${path} --port 5858`)
+        console.log(path)
+    }
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,30 @@
 import React, {useState} from 'react'
+import reactDom from 'react-dom'
+import InputGroup from '../components/InputGroup/InputGroup'
+import FormGroup from '../components/FormGroup/FormGroup'
 import '../styles/home.scss'
+
 
 function Home() {
 
-    function addInput(){
-        console.log('add')
+    const [groups, setGroups] = useState([])
+
+    function addGroup(){
+        let groupContainer = document.querySelector('#formGroups')
+
+        let number = groups.length
+        let newGroup = InputGroup({id: number})
+        groups.push(newGroup)
+        setGroups(groups)
+
+        let Group = FormGroup({groups: groups})
+        reactDom.render(Group, groupContainer)
+    }
+
+    function start(){
+        let path = document.querySelector("#path_group_0").value
+        console.log(path)
+        window.callTool(`${path}`)
     }
 
     return (
@@ -40,11 +60,15 @@ function Home() {
                         </select>
                     </div>
 
+                    <div id="formGroups">
+                        
+                    </div>
 
-
-                    <button className="btn-add" id="btn-add" onClick={addInput}></button>
+                    <button className="btn-add" id="btn-add" onClick={addGroup}></button>
 
                 </div>
+
+                <button className='btn-start' onClick={start}>Start</button>
 
             </div>
 
